7131b95a634683a0bea283de704ad05e8278139d
Hygor Marques <hygor.marques@protonmail.com>
2023-10-01 23:42:14 -0300

    Insertion Merge completed

--- Trabalho/insertionMerge.c
+++ Trabalho/insertionMerge.c
@@ -1,6 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void insertionSort(int[], int);
+void printArray(int[], int);
+
 int main(){
-    
+    int array[]={12,11,13,5,6};
+    int n = sizeof(array) / sizeof(array[0]);
+
+    insertionSort(array, n);
+    printArray(array, n);
+    return 0;
+}
+
+void insertionSort(int array[], int n){
+    int i, key, j;
+
+    for(i=0;i<n;i++){
+        key=array[i];
+        j=i-1;
+
+        while(j>=0 && array[j] > key){
+            array[j+1] = array[j];
+            j=j-1;
+        }
+        array[j+1]=key;
+    }
+}
+
+void printArray(int array[], int n){
+    for(int i=0;i<n;i++)
+        printf("%d ", array[i]);
+    printf("\n");
 }
