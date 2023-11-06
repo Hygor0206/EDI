a408ce4ea8e872ad8b1a53a1016a11e19653e4f4
Hygor Marques <68515062+Hygor0206@users.noreply.github.com>
2023-11-06 12:08:52 -0300

    Add queuePriority file to make an ascending logic to an input

---
+++ filas/filaPrioridade.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "fila.c"
+
+void pqinsert();
+void pqmindelete();
+int empty(Queue*);
+
+int main(){
+
+}
+
+void pqinsert(){
+    
+}
+
+void pqmindelete(){
+
+}
+
+int empty(Queue* queue){
+    return emptyList(queue);
+}
